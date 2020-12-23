0a793dc6274c956f96e7e4ab7fd0050da1e37d23
Jijeong <jijeong0114@gmail.com>
2020-12-24 00:06:02 +0900

    test matchMedia 에러 수정

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders MatchLists", () => {
+  beforeAll(() => {
+    Object.defineProperty(window, "matchMedia", {
+      value: jest.fn(() => {
+        return {
+          matches: true,
+          addListener: jest.fn(),
+          removeListener: jest.fn()
+        };
+      })
+    });
+  });
+
   render(<App />);
   const linkElement = screen.getByText(/신청가능/i);
   expect(linkElement).toBeInTheDocument();
