0bd507a50c46f6e6102c107660ec11f1e28a39ca
Jijeong <jijeong0114@gmail.com>
2020-12-25 16:37:08 +0900

    prettier 적용

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -2,7 +2,7 @@ import "./Label.css";
 import { ReactNode } from "react";
 
 type Type = "primary" | "secondary" | "progress" | "error" | "success";
-type LabelProps = { type: Type, children: ReactNode };
+type LabelProps = { type: Type; children: ReactNode };
 
 const Label = ({ type, children }: LabelProps) => {
   return <span className={`label ${type}`}>{children}</span>;
