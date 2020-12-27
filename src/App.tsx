08fab52eaa6a261f57cbe28630dae756aec0d454
Jijeong <jijeong0114@gmail.com>
2020-12-27 18:40:03 +0900

    user, match 변수 빼서 관리

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import MatchList from "./components/MatchList";
 import PlayerList from "./components/PlayerList";
 import "./App.css";
 import firebase from "firebase";
-
+import { Player, Match } from "./types";
 // Required for side-effects
 require("firebase/firestore");
 // Initialize Cloud Firestore through Firebase
@@ -12,47 +12,50 @@ firebase.initializeApp({
   projectId: "dailyfield-a845d",
 });
 const db = firebase.firestore();
+const player: Player = {
+  name: "이지정",
+  gender: "여성",
+  phoneNumber: "010-9014-3492",
+  birthDate: new Date("1992-01-14"),
+  matchesPlayed: 0,
+};
+const match: Match = {
+  dateTime: new Date(),
+  place: "용산 더베이스",
+  memberCount: 15,
+  teamCount: 3,
+  gender: "여성",
+  level: "초급",
+  link: "naver.com",
+  gameType: "gx",
+  fee: 20000,
+  canPark: true,
+  canRentShoes: false,
+  manager: "배성진",
+};
+const addUser = () => {
+  db.collection("users")
+    .add(player)
+    .then(function (docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function (error) {
+      console.error("Error adding document: ", error);
+    });
+};
+
+const addMatch = () => {
+  db.collection("matches")
+    .add(match)
+    .then(function (docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function (error) {
+      console.error("Error adding document: ", error);
+    });
+};
 
 function App(): JSX.Element {
-  const addUser = () => {
-    db.collection("users")
-      .add({
-        name: "이지정",
-        gender: "여성",
-        birthdate: "1992-01-14",
-      })
-      .then(function (docRef) {
-        console.log("Document written with ID: ", docRef.id);
-      })
-      .catch(function (error) {
-        console.error("Error adding document: ", error);
-      });
-  };
-
-  const addMatch = () => {
-    db.collection("matches")
-      .add({
-        dateTime: new Date(),
-        place: "용산 더베이스",
-        memberCount: 15,
-        teamCount: 3,
-        gender: "여성",
-        level: "초급",
-        link: "naver.com",
-        gameType: "gx",
-        fee: 20000,
-        canPark: true,
-        canRentShoes: false,
-        manager: "배성진",
-      })
-      .then(function (docRef) {
-        console.log("Document written with ID: ", docRef.id);
-      })
-      .catch(function (error) {
-        console.error("Error adding document: ", error);
-      });
-  };
-
   return (
     <div className="App">
       <MatchList />
