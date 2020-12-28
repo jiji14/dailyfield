3cd564df70c117889a56182bc28e52047bf981d6
Jijeong <jijeong0114@gmail.com>
2020-12-28 15:23:14 +0900

    전화번호 인증 로그인 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,59 @@ import React from "react";
 import { Row, Col } from "antd";
 import "antd/dist/antd.css";
 import "./Header.css";
+import firebase from "firebase";
+import { Player } from "../types";
+
+declare global {
+  interface Window {
+    recaptchaVerifier: firebase.auth.ApplicationVerifier;
+    confirmationResult: firebase.auth.ConfirmationResult;
+  }
+}
+
+const signInWithPhoneNumber = () => {
+  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+    "sign-in-button",
+    {
+      size: "invisible",
+      callback: function (response: unknown) {
+        console.log(response);
+      },
+      "expired-callback": function () {
+        console.log("failed");
+      },
+    }
+  );
+
+  const phoneNumber = "+821090143492";
+  const appVerifier = window.recaptchaVerifier;
+  firebase
+    .auth()
+    .signInWithPhoneNumber(phoneNumber, appVerifier)
+    .then(function (confirmationResult) {
+      window.confirmationResult = confirmationResult;
+      const code = window.prompt("코드를 입력해주세요.") || "";
+      confirmationResult.confirm(code).then(function (result) {
+        const user = result.user;
+        const signInUser: Player = {
+          name: "이지정",
+          gender: "여성",
+          phoneNumber: user ? user.phoneNumber || "" : "",
+          birthDate: new Date("1992-01-14"),
+          matchesPlayed: 0,
+        };
+        const db = firebase.firestore();
+        db.collection("users")
+          .doc(user ? user.uid || "" : "")
+          .set({
+            signInUser,
+          });
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
 const Header = (): JSX.Element => {
   return (
@@ -16,7 +69,9 @@ const Header = (): JSX.Element => {
         </Col>
         <Col span={4}></Col>
         <Col span={4} className="login">
-          LOGIN
+          <button onClick={signInWithPhoneNumber} id="sign-in-button">
+            LOGIN
+          </button>
         </Col>
       </Row>
       <div className="banner">새로운 커뮤니티의 시작♡ DAILY FIELD</div>
