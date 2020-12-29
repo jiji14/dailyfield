b9af0cbc8901cca2f299824ecd02f76b8d790ed8
Jijeong <jijeong0114@gmail.com>
2020-12-29 11:38:45 +0900

    console.log 삭제 및 appVerifier 맨 상단으로 올리기

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,8 @@ import "./Header.css";
 import firebase from "firebase";
 import { useHistory } from "react-router-dom";
 
+let appVerifier: firebase.auth.ApplicationVerifier | null = null;
+
 const Header = (): JSX.Element => {
   const history = useHistory();
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -24,44 +26,42 @@ const Header = (): JSX.Element => {
     setPhoneNumber(value);
   };
 
-  let appVerifier: firebase.auth.ApplicationVerifier | null = null;
-
   const signInWithPhoneNumber = () => {
+    console.log(appVerifier);
     if (!appVerifier) {
       appVerifier = new firebase.auth.RecaptchaVerifier("sign-in-button", {
         size: "invisible",
-        callback: function (response: unknown) {
-          console.log(response);
-        },
-        "expired-callback": function () {
-          console.log("failed");
-        },
       });
     }
-
+    console.log(appVerifier);
     if (!appVerifier) return;
-
+    console.log(appVerifier);
     firebase
       .auth()
       .signInWithPhoneNumber(phoneNumber, appVerifier)
       .then(function (confirmationResult) {
         const code = window.prompt("코드를 입력해주세요.") || "";
-        confirmationResult.confirm(code).then(function (result) {
-          const user = result.user;
-          const isNewUSer = result.additionalUserInfo?.isNewUser;
+        confirmationResult
+          .confirm(code)
+          .then(function (result) {
+            const { user } = result;
+            const isNewUser = result.additionalUserInfo?.isNewUser;
 
-          if (isNewUSer) {
-            history.push({
-              pathname: "/signup",
-              state: {
-                user: user,
-              },
-            });
-          } else {
-            setUser(user);
-            setIsModalVisible(false);
-          }
-        });
+            if (isNewUser) {
+              history.push({
+                pathname: "/signup",
+                state: {
+                  user: user,
+                },
+              });
+            } else {
+              setUser(user);
+              setIsModalVisible(false);
+            }
+          })
+          .catch((e) => {
+            window.confirm(e);
+          });
       })
       .catch(function (error) {
         console.log(error);
