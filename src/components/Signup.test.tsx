5423d0e9b099f8fdef71c69ede1985c14dde97e2
Jijeong <jijeong0114@gmail.com>
2020-12-30 18:07:09 +0900

    signup test 추가

--- src/components/Signup.test.tsx
+++ src/components/Signup.test.tsx
@@ -1,10 +1,49 @@
-import { render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import Signup from "./Signup";
+import firebase from "firebase";
+import userEvent from "@testing-library/user-event";
+import { useHistory } from "react-router-dom";
 
 describe("Test", () => {
-  test("renders Signup", () => {
+  beforeEach(() => {
+    ((firebase.auth as unknown) as jest.Mock).mockReturnValue({
+      currentUser: {},
+    });
+
+    ((firebase.firestore as unknown) as jest.Mock).mockReturnValue({
+      collection: jest
+        .fn()
+        .mockReturnValue({ add: jest.fn().mockResolvedValue(null) }),
+    });
+    window.alert = () => "";
+  });
+
+  test("Signup", async () => {
     render(<Signup />);
-    const button = screen.getByText(/가입하기/i);
-    expect(button).toBeInTheDocument();
+    const name = screen.getByPlaceholderText("이름을 입력해주세요.");
+    fireEvent.change(name, { target: { value: "TEST" } });
+
+    await act(async () => {
+      fireEvent.mouseDown(screen.getByTestId("signUpSelect").firstElementChild);
+    });
+
+    await act(async () => {
+      const women = await screen.findByTitle("여성");
+      fireEvent.click(women);
+    });
+
+    const sporty = screen.getByDisplayValue("sporty");
+    userEvent.click(sporty);
+
+    const signupButton = screen.getByText(/가입하기/i);
+    await act(async () => {
+      fireEvent.click(signupButton);
+    });
+    const user = firebase.firestore().collection("user").add.mock.calls[0][0];
+    expect(user.name).toBe("TEST");
+    expect(user.gender).toBe("여성");
+    expect(user.theme).toStrictEqual(["sporty"]);
+    expect(user.matchesPlayed).toBe(0);
+    expect(useHistory().push.mock.calls[0][0]).toBe("/");
   });
 });
