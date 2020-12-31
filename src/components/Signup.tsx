14f65f365e49a49752d97ad306d603848b7e2143
Jijeong <jijeong0114@gmail.com>
2020-12-31 22:57:53 +0900

    코드 개선사항 반영 (type error, db.add -> db.aset)

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -15,52 +15,60 @@ import Header from "./Header";
 import moment, { Moment } from "moment";
 import firebase from "firebase";
 import { CheckboxValueType } from "antd/lib/checkbox/Group";
-import { Player } from "../types";
+import { Player, Gender } from "../types";
 import { useHistory } from "react-router-dom";
 
 const { Option } = Select;
+const Options = [
+  { label: "스포티", value: "sporty" },
+  { label: "취미", value: "hobby" },
+  { label: "초급", value: "beginner" },
+];
 
 const Signup = (): JSX.Element => {
   const history = useHistory();
-  const options = [
-    { label: "스포티", value: "sporty" },
-    { label: "취미", value: "hobby" },
-    { label: "초급", value: "beginner" },
-  ];
   const [name, setName] = useState("");
-  const [birthDate, setBirthDate] = useState<Moment>(moment);
-  const [gender, setGender] = useState<"남성" | "여성">("남성");
-  const [theme, setTheme] = useState<CheckboxValueType[]>([]);
+  const [birthDate, setBirthDate] = useState<Moment | null>(null);
+  const [gender, setGender] = useState<Gender>("남성");
+  const [purpose, setPurpose] = useState<string[]>([]);
 
   const changeName = (e: React.FormEvent<HTMLInputElement>) => {
     setName(e.currentTarget.value);
   };
 
-  const changeBirthdate = (dateObj: Moment | null) => {
-    setBirthDate(moment(dateObj ? dateObj : birthDate));
-  };
-
-  const changeGender = (gender: "남성" | "여성") => {
-    setGender(gender);
-  };
-
-  const changeTheme = (checkedValue: CheckboxValueType[]) => {
-    setTheme(checkedValue);
+  const changePurpose = (checkedValue: CheckboxValueType[]) => {
+    setPurpose(checkedValue as string[]);
   };
 
   const signUp = () => {
-    const phoneNumber = firebase.auth().currentUser?.phoneNumber || "";
+    if (name.length < 1) {
+      window.alert("이름을 입력해주세요.");
+      return;
+    }
+
+    if (!birthDate) {
+      window.alert("생일을 입력해주세요.");
+      return;
+    }
+
+    const currentUser = firebase.auth().currentUser;
+    if (!currentUser) {
+      window.alert("인증절차를 진행후 회원가입을 진행해주세요.");
+      return;
+    }
+
     const user: Player = {
       name: name,
       gender: gender,
-      phoneNumber: phoneNumber,
-      birthDate: birthDate.toDate(),
+      phoneNumber: currentUser?.phoneNumber || "",
+      birthDate: birthDate?.toDate() || null,
       matchesPlayed: 0,
-      theme: theme,
+      purpose: purpose,
     };
     const db = firebase.firestore();
     db.collection("users")
-      .add(user)
+      .doc(currentUser.uid)
+      .set(user)
       .then(function () {
         window.alert("회원가입을 축하합니다!");
         history.push("/");
@@ -71,72 +79,68 @@ const Signup = (): JSX.Element => {
   };
 
   return (
-    <div>
-      <Header />
-      <div className="signUp">
-        <section className="signUpContainer">
-          <h3>필수사항</h3>
-          <Divider className="divider" />
-          <Row align="middle" className="Row">
-            <Col span={6} className="signUpSubtitle">
-              이름
-            </Col>
-            <Col span={18}>
-              <Input
-                onChange={changeName}
-                value={name}
-                placeholder="이름을 입력해주세요."
-              />
-            </Col>
-          </Row>
-          <Row align="middle" className="Row">
-            <Col span={6} className="signUpSubtitle">
-              생년월일
-            </Col>
-            <Col span={18}>
-              <DatePicker onChange={changeBirthdate} value={birthDate} />
-            </Col>
-          </Row>
-        </section>
-        <section className="signUpContainer">
-          <h3>선택사항</h3>
-          <Divider className="divider" />
-          <Row align="middle" className="Row">
-            <Col span={6} className="signUpSubtitle">
-              성별
-            </Col>
-            <Col span={18}>
-              <Select
-                value={gender}
-                onChange={changeGender}
-                className="signUpSelect"
-                data-testid="signUpSelect"
-              >
-                <Option value="남성">남성</Option>
-                <Option value="여성">여성</Option>
-              </Select>
-            </Col>
-          </Row>
-          <Row align="middle" className="Row">
-            <Col span={6} className="signUpSubtitle">
-              테마
-            </Col>
-            <Col span={18}>
-              <div className="signUpSubtitle"> 중복선택 가능합니다.</div>
-              <Checkbox.Group
-                options={options}
-                onChange={changeTheme}
-                value={theme}
-                data-testid="signUpCheckbox"
-              />
-            </Col>
-          </Row>
-        </section>
-        <div className="signUpButtonContainer">
-          <Button type="primary" onClick={signUp}>
-            가입하기
-          </Button>
-        </div>
+    <div className="signUp">
+      <section className="signUpContainer">
+        <h3>필수사항</h3>
+        <Divider className="divider" />
+        <Row align="middle" className="Row">
+          <Col span={6} className="signUpSubtitle">
+            이름
+          </Col>
+          <Col span={18}>
+            <Input
+              onChange={changeName}
+              value={name}
+              placeholder="이름을 입력해주세요."
+            />
+          </Col>
+        </Row>
+        <Row align="middle" className="Row">
+          <Col span={6} className="signUpSubtitle">
+            생년월일
+          </Col>
+          <Col span={18}>
+            <DatePicker onChange={setBirthDate} value={birthDate} />
+          </Col>
+        </Row>
+      </section>
+      <section className="signUpContainer">
+        <h3>선택사항</h3>
+        <Divider className="divider" />
+        <Row align="middle" className="Row">
+          <Col span={6} className="signUpSubtitle">
+            성별
+          </Col>
+          <Col span={18}>
+            <Select
+              value={gender}
+              onChange={setGender}
+              className="signUpSelect"
+              data-testid="signUpSelect"
+            >
+              <Option value="남성">남성</Option>
+              <Option value="여성">여성</Option>
+            </Select>
+          </Col>
+        </Row>
+        <Row align="middle" className="Row">
+          <Col span={6} className="signUpSubtitle">
+            테마
+          </Col>
+          <Col span={18}>
+            <div className="signUpSubtitle"> 중복선택 가능합니다.</div>
+            <Checkbox.Group
+              options={Options}
+              onChange={changePurpose}
+              value={purpose}
+            />
+          </Col>
+        </Row>
+      </section>
+      <div className="signUpButtonContainer">
+        <Button type="primary" onClick={signUp}>
+          가입하기
+        </Button>
       </div>
     </div>
   );
