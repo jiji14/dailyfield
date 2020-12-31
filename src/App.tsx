801595ff304dd38424712cbf4a4703cd0074fd93
Jijeong <jijeong0114@gmail.com>
2020-12-31 22:56:18 +0900

    Header를 Router 밖으로 빼기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import MatchList from "./components/MatchList";
 import PlayerList from "./components/PlayerList";
-import Header from "./components/Header";
 import "./App.css";
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <Header />
       <MatchList />
       <PlayerList />
     </div>
