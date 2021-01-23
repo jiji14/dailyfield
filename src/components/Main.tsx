125adc01f9337ac47903f8bab90e89e89e4161bd
Jijeong <jijeong0114@gmail.com>
2021-01-23 19:29:29 +0900

    필요없는 모듈 삭제, MatchListItem에 key값 주기

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "antd/dist/antd.css";
 import "./Main.css";
-import { Divider, Row, Col } from "antd";
-import Label from "./Label";
+import { Divider } from "antd";
 import firebase from "firebase";
-import MatchList from "./MatchList";
+import MatchListItem from "./MatchListItem";
 import { Match } from "../types";
 
 const Main = (): JSX.Element => {
@@ -37,11 +36,7 @@ const Main = (): JSX.Element => {
       <Divider className="divider" />
       <section className="signUpContainer">
         {matchlists.map((match, idx) => {
-          return (
-            <div key={match + "idx"}>
-              <MatchList match={match} />
-            </div>
-          );
+          return <MatchListItem key={"match" + idx} match={match} />;
         })}
       </section>
     </div>
