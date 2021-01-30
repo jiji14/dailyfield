1e4a163b167687a19910dbfcd74fffcb9bf49d70
Jijeong <jijeong0114@gmail.com>
2021-01-30 14:45:46 +0900

    css 수정, antd Tag 사용

--- src/components/MatchDetail.tsx
+++ src/components/MatchDetail.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Divider, Button } from "antd";
+import { Divider, Button, Tag } from "antd";
 import "antd/dist/antd.css";
 import "./MatchDetail.css";
 import { Match } from "../types";
@@ -34,124 +34,116 @@ const MatchDetail = (): JSX.Element => {
         목록으로 돌아가기
       </a>
       <Divider className="divider" />
+      <div className="container">
+        <div>{match.dateTime?.toDateString()}</div>
+        <Label type="progress">마감</Label>
+      </div>
+      <div className="container">
+        <div>{match.place}</div>
+        <a href={match.link} target="_blank">
+          구장 정보보기
+        </a>
+      </div>
+      <div className="tagContainer">
+        <Tag color="warning" className="chip">
+          {match.gender}매치
+        </Tag>
+        <Tag color="warning" className="chip">
+          최대 {match.memberCount}명 ({match.teamCount}파전)
+        </Tag>
+        <Tag color="warning" className="chip">
+          {match.level}레벨
+        </Tag>
+        <Tag color="warning" className="chip">
+          {match.gameType}
+        </Tag>
+        <Tag color="warning" className="chip">
+          풋살화 대여 {match.canRentShoes ? "가능" : "불가능"}
+        </Tag>
+        <Tag color="warning" className="chip">
+          주차 {match.canPark ? "가능" : "불가능"}
+        </Tag>
+      </div>
+      <p className="details">* 여성매치시 인원부족시 혼성매치로 진행됩니다.</p>
       <section>
-        <div className="container row">
-          <div>{match.dateTime?.toDateString()}</div>
-          <Label type="progress">마감</Label>
-        </div>
-        <div className="container row">
-          <div>{match.place}</div>
-          <a href={match.link} target="_blank">
-            구장 정보보기
-          </a>
-        </div>
-        <div className="container">
-          <div className="chip">{match.gender}매치</div>
-          <div className="chip">
-            최대 {match.memberCount}명 ({match.teamCount}파전)
-          </div>
-          <div className="chip">{match.level}레벨</div>
-        </div>
-        <div className="container">
-          <div className="chip">{match.gameType}</div>
-          <div className="chip">
-            풋살화 대여 {match.canRentShoes ? "가능" : "불가능"}
-          </div>
-          <div className="chip">주차 {match.canPark ? "가능" : "불가능"}</div>
-        </div>
+        <h3 className="title">참가비</h3>
         <p className="details">
-          * 여성매치시 인원부족시 혼성매치로 진행됩니다.
+          {match.fee.toLocaleString()}원 (첫참석시{" "}
+          {(match.fee / 2).toLocaleString()}원)
         </p>
-        <div>
-          <h3 className="title">참가비</h3>
-          <p className="details">
-            {match.fee.toLocaleString()}원 (첫참석시{" "}
-            {(match.fee / 2).toLocaleString()}원)
-          </p>
-        </div>
-        <div>
-          <h3 className="title">매니저</h3>
-          <p className="details">{match.manager}</p>
-        </div>
-        <div className="flex">
-          <h3 className="title">매치 안내</h3>
-          <p className="details">
-            <h4>준비물</h4>
-            <ul>
-              <li>신발 복장은 편한것으로 준비</li>
-              <li>열정적이고 열린 마음 준비</li>
-            </ul>
-          </p>
-          <p className="details">
-            <h4>프로그램 안내</h4>
-            <ul>
-              <li>스타일 : GX 프로그램(30분) + 매치(1시간 30분)</li>
-              <li>
-                GX프로그램은 데일리필드만의 차별화된 진행 방식입니다. 인트로에
-                짧게 프로그램을 진행함으로써,
-              </li>
-            </ul>
-            <ol>
-              <li>
-                초급분들께는 풋살에 필요한 기본 스킬을 익힐수 있는 방향을 제시해
-                드려요!
-              </li>
-              <li>
-                혼자 또는 친구분들과 참석해도 어색하지 않아요! 처음보는 팀원들과
-                친해지고 소통할수 있는 기회를 제공합니다.
-              </li>
-              <li>준비된 몸과 마음을 매치 플레이에 적극적으로 펼치세요!^^</li>
-            </ol>
-          </p>
-        </div>
-        <div className="flex">
-          <h3 className="title">예약 안내</h3>
-          <p className="details">
-            <ol>
-              <li>
-                언제까지 예약 가능한지 : 최대인원 {match.memberCount}명 모집시
-              </li>
-              <li>
-                언제까지 예약취소 가능한지 : 서비스의 질을 고려하여 당일취소는
-                환급이 어렵습니다.
-              </li>
-              <li className="subLi">
-                <ul>
-                  <li>하루전 취소시 50% 환급</li>
-                  <li>2일전 취소시 : 100% 환급</li>
-                </ul>
-              </li>
-            </ol>
-            <ul>
-              <li>상대방을 배려하는 측면에서 참고해주시면 감사합니다.</li>
-              <li>기타 자연재해 등으로 인한 운영불가 시 100% 환급 드립니다.</li>
-              <li>
-                최소인원 미달시 매치취소 안내와 함께 참가비를 전액 환급드립니다.
-              </li>
-            </ul>
-          </p>
-        </div>
-        <div className="flex">
-          <h3 className="title">입금 절차</h3>
-          <p className="details">
-            <ul>
-              <li>
-                예약하기 버튼 클릭 후 아래계좌로 참가비를 입금해주시면 매니저
-                확인 후 예약확정이 됩니다.
-              </li>
-              <li>국민은행 472501-04-011482 배성진</li>
-            </ul>
-          </p>
-        </div>
-        <div>
-          <ul className="iconUl">
-            <li>축구도 이제 고급스포츠! 혼자와도 재밌게 운동하자!</li>
-            <li>새로운 커뮤니티의 시작♡ 데일리필드⚽</li>
-            <li>인스타팔로우 @daily_field</li>
-            <li>블로그♡ blog.naver.com/piterq</li>
-            <li>스페셜 영상 youtu.be/08zp7lrtOpc</li>
+        <h3 className="title">매니저</h3>
+        <p className="details">{match.manager}</p>
+        <h3 className="title">매치 안내</h3>
+        <p className="details">
+          <h4>준비물</h4>
+          <ul>
+            <li>신발 복장은 편한것으로 준비</li>
+            <li>열정적이고 열린 마음 준비</li>
           </ul>
-        </div>
+        </p>
+        <p className="details">
+          <h4>프로그램 안내</h4>
+          <ul>
+            <li>스타일 : GX 프로그램(30분) + 매치(1시간 30분)</li>
+            <li>
+              GX프로그램은 데일리필드만의 차별화된 진행 방식입니다. 인트로에
+              짧게 프로그램을 진행함으로써,
+            </li>
+          </ul>
+          <ol>
+            <li>
+              초급분들께는 풋살에 필요한 기본 스킬을 익힐수 있는 방향을 제시해
+              드려요!
+            </li>
+            <li>
+              혼자 또는 친구분들과 참석해도 어색하지 않아요! 처음보는 팀원들과
+              친해지고 소통할수 있는 기회를 제공합니다.
+            </li>
+            <li>준비된 몸과 마음을 매치 플레이에 적극적으로 펼치세요!^^</li>
+          </ol>
+        </p>
+        <h3 className="title">예약 안내</h3>
+        <p className="details">
+          <ol>
+            <li>
+              언제까지 예약 가능한지 : 최대인원 {match.memberCount}명 모집시
+            </li>
+            <li>
+              언제까지 예약취소 가능한지 : 서비스의 질을 고려하여 당일취소는
+              환급이 어렵습니다.
+            </li>
+            <li className="subLi">
+              <ul>
+                <li>하루전 취소시 50% 환급</li>
+                <li>2일전 취소시 : 100% 환급</li>
+              </ul>
+            </li>
+          </ol>
+          <ul>
+            <li>상대방을 배려하는 측면에서 참고해주시면 감사합니다.</li>
+            <li>기타 자연재해 등으로 인한 운영불가 시 100% 환급 드립니다.</li>
+            <li>
+              최소인원 미달시 매치취소 안내와 함께 참가비를 전액 환급드립니다.
+            </li>
+          </ul>
+        </p>
+        <h3 className="title">입금 절차</h3>
+        <p className="details">
+          <ul>
+            <li>
+              예약하기 버튼 클릭 후 아래계좌로 참가비를 입금해주시면 매니저 확인
+              후 예약확정이 됩니다.
+            </li>
+            <li>국민은행 472501-04-011482 배성진</li>
+          </ul>
+        </p>
+        <ul className="iconUl">
+          <li>축구도 이제 고급스포츠! 혼자와도 재밌게 운동하자!</li>
+          <li>새로운 커뮤니티의 시작♡ 데일리필드⚽</li>
+          <li>인스타팔로우 @daily_field</li>
+          <li>블로그♡ blog.naver.com/piterq</li>
+          <li>스페셜 영상 youtu.be/08zp7lrtOpc</li>
+        </ul>
       </section>
       <Divider className="divider" />
       <div className="signUpButtonContainer">
