4d23154f9fe33be64c34acba8911068c20338fce
Jijeong <jijeong0114@gmail.com>
2021-01-31 17:35:13 +0900

    matchList 날짜별로 보여주기

--- src/components/MatchList.tsx
+++ src/components/MatchList.tsx
@@ -8,6 +8,8 @@ import { Match } from "../types";
 
 const Main = (): JSX.Element => {
   const [matches, setMatches] = useState<Match[]>([]);
+  const [matchDate, setMatchDate] = useState<string[]>([]);
+
   useEffect(() => {
     getMatches();
   }, []);
@@ -18,22 +20,59 @@ const Main = (): JSX.Element => {
       .collection("matches")
       .orderBy("dateTime", "desc")
       .get();
+
+    const matchDateArray: Array<string> = [];
     const matches: Match[] = querySnapshot.docs.map((doc) => {
       const data = doc.data();
       data.dateTime = data.dateTime.toDate();
+      const day = data.dateTime.getDate();
+      const month = data.dateTime.getMonth() + 1;
+      const year = data.dateTime.getFullYear();
+      const dayOfWeek = getDayOfWeek(data.dateTime.getDay());
+      matchDateArray.push(year + "-" + month + "-" + day + " " + dayOfWeek);
       data.id = doc.id;
       return data as Match;
     });
+    setMatchDate(matchDateArray);
     setMatches(matches);
+  };
+
+  const getDayOfWeek = (dayOfWeek: number) => {
+    switch (dayOfWeek) {
+      case 1:
+        return "월요일";
+      case 2:
+        return "화요일";
+      case 3:
+        return "수요일";
+      case 4:
+        return "목요일";
+      case 5:
+        return "금요일";
+      case 6:
+        return "토요일";
+      case 0:
+        return "일요일";
+      default:
+        return null;
+    }
   };
 
   return (
     <div className="matchList">
       <h2>매치목록</h2>
-      <Divider className="divider" />
       <section className="matchListContainer">
         {matches.map((match, idx) => {
-          return <MatchListItem key={"match" + idx} match={match} />;
+          if (matchDate[idx] !== matchDate[idx - 1]) {
+            return (
+              <div>
+                <div className="dateTitle">{matchDate[idx]}</div>
+                <MatchListItem key={"match" + idx} match={match} />
+              </div>
+            );
+          } else {
+            return <MatchListItem key={"match" + idx} match={match} />;
+          }
         })}
       </section>
     </div>
