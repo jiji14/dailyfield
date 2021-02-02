7f6da28c33c1f1ed7caa65b4b4ae45cd132be5a3
Jijeong <jijeong0114@gmail.com>
2021-02-03 00:07:10 +0900

    핸드폰번호 받아서 양식에 맞게 바꾸기

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,6 +27,15 @@ const Header = (): JSX.Element => {
   };
 
   const signInWithPhoneNumber = async () => {
+    const regExp = /^01(?:0|1|[6-9])(?:\d{7}|\d{8})$/;
+    if (!regExp.test(phoneNumber)) {
+      window.alert("올바른 번호가 아닙니다. 다시한번 확인해주세요.");
+      return;
+    }
+
+    const proccessedPhoneNumber =
+      "+82" + phoneNumber.substring(1, phoneNumber.length);
+
     if (!appVerifier) {
       appVerifier = new firebase.auth.RecaptchaVerifier("sign-in-button", {
         size: "invisible",
@@ -36,7 +45,7 @@ const Header = (): JSX.Element => {
     try {
       const confirmationResult = await firebase
         .auth()
-        .signInWithPhoneNumber(phoneNumber, appVerifier);
+        .signInWithPhoneNumber(proccessedPhoneNumber, appVerifier);
       const code = window.prompt("코드를 입력해주세요.") || "";
       const { user } = await confirmationResult.confirm(code);
       const db = firebase.firestore();
@@ -78,6 +87,7 @@ const Header = (): JSX.Element => {
             <button
               onClick={showModal}
               id="sign-in-button"
+              data-testid="sign-in-button"
               className="headerButton"
             >
               SIGNIN
@@ -102,8 +112,19 @@ const Header = (): JSX.Element => {
         <input
           value={phoneNumber}
           onChange={changePhoneNumber}
-          placeholder="핸드폰번호"
+          className="phoneNumber"
+          placeholder="- 없이 숫자만 입력해주세요. (ex)01012345678"
         />
+        <ul>
+          <li>
+            핸드폰번호 입력 후 로그인 버튼을 클릭하면 인증번호가 발송됩니다.
+            인증번호 입력후 로그인 하실 수 있습니다.
+          </li>
+          <li>
+            신규회원인 경우 회원가입 페이지로 전환되니 회원가입을 진행후
+            로그인해주세요.
+          </li>
+        </ul>
       </Modal>
     </div>
   );
