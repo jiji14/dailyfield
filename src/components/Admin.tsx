d1cff17925261087b5481d9b53ceca6aa68b9c8e
Jijeong <jijeong0114@gmail.com>
2021-02-12 13:01:57 +0900

    관리자페이지 선수목록 추가

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,5 +1,78 @@
+import React, { useState, useEffect } from "react";
+import "antd/dist/antd.css";
+import "./Admin.css";
+import firebase from "firebase";
+import { Divider } from "antd";
+import { Link, useHistory, useParams } from "react-router-dom";
+import PlayerList from "./PlayerList";
+
 const Admin = (): JSX.Element => {
-  return <div>관리자페이지</div>;
+  const history = useHistory();
+  const { id } = useParams<{ id: string }>();
+  const [user, setUser] = useState(firebase.auth().currentUser);
+  const [players, setPlayers] = useState<Map<string, string>>(new Map());
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(setUser);
+  }, []);
+
+  useEffect(() => {
+    async function getUser() {
+      if (user) {
+        const db = firebase.firestore();
+        const doc = await db.collection("users").doc(user.uid).get();
+        if (doc.exists) {
+          if (!doc.data()?.isAdmin) {
+            window.alert("관리자로 로그인해주세요.");
+            history.push("/");
+            return;
+          }
+        }
+      }
+    }
+
+    getUser();
+  }, [user, history]);
+
+  useEffect(() => {
+    async function getPlayers() {
+      const db = firebase.firestore();
+      const querySnapshot = await db
+        .collection("matches")
+        .doc(id)
+        .collection("reservation")
+        .get();
+      const players: Map<string, string> = new Map();
+      querySnapshot.docs.forEach((doc) => {
+        const id = doc.id;
+        const status = doc.data().status;
+        players.set(id, status);
+      });
+      setPlayers(players);
+    }
+    getPlayers();
+  }, [id]);
+
+  const renderPlayerlists = () => {
+    return [...players].map(([playerKey, status]) => {
+      return <PlayerList key={playerKey} id={playerKey} status={status} />;
+    });
+  };
+
+  return (
+    <div className="admin">
+      <Link to="/">목록으로 돌아가기</Link>
+      <Divider className="divider" />
+      <h2>회원목록</h2>
+      <section className="playerListContainer">
+        {players.size > 0 ? (
+          renderPlayerlists()
+        ) : (
+          <div>현재 예약중인 회원이 없습니다.</div>
+        )}
+      </section>
+    </div>
+  );
 };
 
 export default Admin;
