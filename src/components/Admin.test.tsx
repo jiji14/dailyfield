101de33c730d1f7bf2c46965bd5851cd34646b5e
Jijeong <jijeong0114@gmail.com>
2021-02-12 19:07:18 +0900

    관리자페이지에서 매치 정보 및 수정/삭제 버튼 나오는지 테스트 추가

--- src/components/Admin.test.tsx
+++ src/components/Admin.test.tsx
@@ -1,9 +1,45 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Admin from "./Admin";
+import firebase from "firebase";
 
 describe("Test", () => {
+  beforeEach(() => {
+    ((firebase.firestore as unknown) as jest.Mock).mockReturnValue({
+      collection: jest.fn().mockReturnValue({
+        doc: jest.fn().mockReturnValue({
+          get: jest.fn().mockResolvedValue({
+            exists: true,
+            data: jest.fn().mockImplementation(() => {
+              return {
+                dateTime: {
+                  toDate: jest.fn().mockReturnValue(new Date("2021-01-01")),
+                },
+                place: "용산 더베이스",
+                memberCount: 15,
+                teamCount: 3,
+                gender: "여성",
+                level: "초급",
+                link: "www.naver.com",
+                gameType: "match",
+                fee: 20000,
+                canPark: true,
+                canRentShoes: true,
+                manager: "이지정",
+              };
+            }),
+            id: "test12345",
+          }),
+        }),
+      }),
+    });
+  });
+
   test("Admin Page", async () => {
     render(<Admin />);
-    expect(screen.getByText("관리자페이지")).toBeInTheDocument();
+    await waitFor(async () => {
+      expect(screen.getByDisplayValue("이지정")).toBeInTheDocument();
+      expect(screen.getByText("수정하기")).toBeInTheDocument();
+      expect(screen.getByText("삭제하기")).toBeInTheDocument();
+    });
   });
 });
