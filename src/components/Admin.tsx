2d3b3cab0c975ae5f995c549d86cfe0eaf8a35b0
Jijeong <jijeong0114@gmail.com>
2021-02-12 19:02:45 +0900

    관리자페이지에 AddMatch 추가

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,5 +1,17 @@
+import "antd/dist/antd.css";
+import { useParams } from "react-router-dom";
+import AddMatch from "./AddMatch";
+
 const Admin = (): JSX.Element => {
-  return <div>관리자페이지</div>;
+  const { id } = useParams<{ id: string }>();
+
+  return (
+    <div className="admin">
+      <section>
+        <AddMatch id={id} />
+      </section>
+    </div>
+  );
 };
 
 export default Admin;
