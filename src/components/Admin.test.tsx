b24370180cac730ea4c9f76fdecbb4deac5eb30d
Jijeong <jijeong0114@gmail.com>
2021-02-12 13:02:34 +0900

    선수목록 잘 나오는지 테스트 추가

--- src/components/Admin.test.tsx
+++ src/components/Admin.test.tsx
@@ -1,9 +1,57 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Admin from "./Admin";
+import firebase from "firebase";
 
 describe("Test", () => {
+  beforeEach(() => {
+    const fakeUser = {} as firebase.User;
+    ((firebase.auth as unknown) as jest.Mock).mockReturnValue({
+      currentUser: {},
+      onAuthStateChanged: (callback: (user: firebase.User) => void) => {
+        callback(fakeUser);
+      },
+    });
+
+    ((firebase.firestore as unknown) as jest.Mock).mockReturnValue({
+      collection: jest.fn().mockReturnValue({
+        doc: jest.fn().mockReturnValue({
+          get: jest.fn().mockResolvedValue({
+            exists: true,
+            data: jest.fn().mockReturnValue({
+              isAdmin: true,
+              birthDate: {
+                toDate: jest.fn().mockReturnValue(new Date("2021-01-01")),
+              },
+              name: "이지정",
+              gender: "여성",
+              phoneNumber: "+821090143492",
+              matchesPlayed: 0,
+              purpose: [],
+            }),
+            id: "test12345",
+          }),
+          collection: jest.fn().mockReturnValue({
+            get: jest.fn().mockResolvedValue({
+              docs: [
+                {
+                  data: jest.fn().mockReturnValue({
+                    status: "예약신청",
+                  }),
+                  id: "test12345",
+                },
+              ],
+            }),
+          }),
+        }),
+      }),
+    });
+  });
+
   test("Admin Page", async () => {
     render(<Admin />);
-    expect(screen.getByText("관리자페이지")).toBeInTheDocument();
+    await waitFor(async () => {
+      expect(screen.getByText("01090143492")).toBeInTheDocument(); //전화번호 양식 제대로 나오는지 확인
+      expect(screen.getByText("신청승인")).toBeInTheDocument(); //예약신청일때 신청승인 버튼 나오는지 확인
+    });
   });
 });
