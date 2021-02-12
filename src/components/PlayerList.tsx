6108b314170f3500b8d6019f5072b73bab87dc37
Jijeong <jijeong0114@gmail.com>
2021-02-12 13:02:21 +0900

    선수정보 firebase에서 가져오기

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col, Divider, Button } from "antd";
 import "antd/dist/antd.css";
 import "./PlayerList.css";
+import firebase from "firebase";
 import { Player } from "../types";
 
-const player: Player = {
-  name: "이지정",
-  gender: "여성",
-  phoneNumber: "010-9014-3492",
-  birthDate: new Date("1992-01-14"),
-  matchesPlayed: 10,
-  status: "예약신청",
-};
+const PlayerList = (playerProps: {
+  id: string;
+  status: string;
+}): JSX.Element => {
+  const { id, status } = playerProps;
+  const [player, setPlayer] = useState<Player | null>(null);
 
-const PlayerList = (): JSX.Element => {
-  return (
+  useEffect(() => {
+    async function getPlayer() {
+      const db = firebase.firestore();
+      const doc = await db.collection("users").doc(id).get();
+      if (!doc.exists) return;
+
+      const user = doc.data();
+      if (user) {
+        user.birthDate = user.birthDate.toDate();
+        user.id = doc.id;
+        user.status = status;
+        setPlayer(user as Player);
+      }
+    }
+    getPlayer();
+  }, [id, status]);
+
+  return player ? (
     <>
       <Row align="middle">
-        <Col span={8} className="playerContainer">
+        <Col span={4} className="playerContainer">
           <div className="playerName">{player.name}</div>
+        </Col>
+        <Col span={4} className="playerContainer">
           <div>{player.gender}</div>
         </Col>
-        <Col span={8}>{player.phoneNumber}</Col>
+        <Col span={8}>
+          {"0" + player.phoneNumber.substring(3, player.phoneNumber.length)}
+        </Col>
         <Col span={8} className="buttonContainer">
           {player.status !== "확정" && (
             <Button type="primary" size="small">
@@ -29,7 +48,7 @@ const PlayerList = (): JSX.Element => {
             </Button>
           )}
           <Button size="small">
-            {player.status === "확정" ? "거절" : "강퇴"}
+            {player.status === "확정" ? "확정취소" : "거절"}
           </Button>
         </Col>
       </Row>
@@ -38,6 +57,8 @@ const PlayerList = (): JSX.Element => {
       </Row>
       <Divider className="divider" />
     </>
+  ) : (
+    <></>
   );
 };
 
