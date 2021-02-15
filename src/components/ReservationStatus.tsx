b80364afeaa4e4836692510aafed568e46495634
Jijeong <jijeong0114@gmail.com>
2021-02-15 17:07:46 +0900

    switch문 바로 돌리기

--- src/components/ReservationStatus.tsx
+++ src/components/ReservationStatus.tsx
@@ -3,27 +3,23 @@ import Label from "./Label";
 
 const ReservationStatus = (props: {
   reservationStatus: Status;
-}): JSX.Element => {
+}): JSX.Element | null => {
   const { reservationStatus } = props;
 
-  const renderReservationStatus = () => {
-    switch (reservationStatus) {
-      case "신청가능":
-        return <Label type="primary">신청가능</Label>;
-      case "예약신청":
-        return <Label type="progress">신청중</Label>;
-      case "마감":
-        return <Label type="secondary">마감</Label>;
-      case "확정":
-        return <Label type="success">예약확정</Label>;
-      case "취소신청":
-        return <Label type="progress">취소중</Label>;
-      default:
-        return null;
-    }
-  };
-
-  return <>{renderReservationStatus()}</>;
+  switch (reservationStatus) {
+    case "신청가능":
+      return <Label type="primary">신청가능</Label>;
+    case "예약신청":
+      return <Label type="progress">신청중</Label>;
+    case "마감":
+      return <Label type="secondary">마감</Label>;
+    case "확정":
+      return <Label type="success">예약확정</Label>;
+    case "취소신청":
+      return <Label type="progress">취소중</Label>;
+    default:
+      return null;
+  }
 };
 
 export default ReservationStatus;
