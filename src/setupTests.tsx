3e1ed71e50e2271435db28acb916736cd06fcaad
Jijeong <jijeong0114@gmail.com>
2021-02-15 21:38:46 +0900

    주석 삭제

--- src/setupTests.tsx
+++ src/setupTests.tsx
@@ -3,7 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-// eslint-disable-next-line no-unused-vars
 import { act, fireEvent, screen } from "@testing-library/react";
 
 global.matchMedia =
