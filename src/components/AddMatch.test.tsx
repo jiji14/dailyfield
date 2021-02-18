fbfe04cd2c3c629f924b381da7dd97f61785560a
Jijeong <jijeong0114@gmail.com>
2021-02-18 12:54:40 +0900

    매치 등록시 불필요한 Match 정보 정리

--- src/components/AddMatch.test.tsx
+++ src/components/AddMatch.test.tsx
@@ -25,12 +25,7 @@ describe("Test", () => {
     await fireAntEvent.actAndSetDate(screen.getByTestId("gameDate"), "Now");
     await fireAntEvent.actAndInput("경기장을 입력해주세요.", "용산 더베이스");
     await fireAntEvent.actAndInput("멤버수를 입력해주세요.", "20");
-    await fireAntEvent.actAndSelect(
-      screen.getByTestId("teamCountSelect"),
-      "3파전"
-    );
     await fireAntEvent.actAndSelect(screen.getByTestId("genderSelect"), "혼성");
-    await fireAntEvent.actAndSelect(screen.getByTestId("levelSelect"), "중급");
     await fireAntEvent.actAndInput(
       "링크를 입력해주세요.",
       "www.dailyfield.info"
@@ -41,7 +36,6 @@ describe("Test", () => {
     );
     await fireAntEvent.actAndSelect(screen.getByTestId("feeSelect"), "4만원");
     await fireAntEvent.actAndCheckbox("canPark");
-    await fireAntEvent.actAndCheckbox("canRentShoes");
     await fireAntEvent.actAndInput("매니저를 입력해주세요.", "배성진");
     await fireAntEvent.actAndClick("등록하기");
 
@@ -50,14 +44,11 @@ describe("Test", () => {
       .collection(CollectionName.matchesCollectionName).add.mock.calls[0][0];
     expect(match.place).toBe("용산 더베이스"); // 장소가 "용산 더베이스"이 맞는지 확인
     expect(match.memberCount).toBe(20); // 인원수가 20이 맞는지 확인
-    expect(match.teamCount).toBe(3); // 팀형태가 3파전이 맞는지 확인
     expect(match.gender).toBe("혼성"); // 성별이 혼성이 맞는지 확인
-    expect(match.level).toBe("중급"); // 레벨이 중급이 맞는지 확인
     expect(match.link).toBe("www.dailyfield.info"); // 링크가 "www.dailyfield.info" 맞는지 확인
-    expect(match.gameType).toBe("match"); // 게입타입이 match가 맞는지 확인
+    expect(match.gameType).toBe("match"); // 게임타입이 match가 맞는지 확인
     expect(match.fee).toBe(40000); // 참가비가 4만원인지 확인
     expect(match.canPark).toBe(false); // 주차 불가능이 맞는지 확인
-    expect(match.canRentShoes).toBe(true); // 풋살화 대여 가능이 맞는지 확인
     expect(match.manager).toBe("배성진"); // 매니저가 "배성진"이 맞는지 확인
     expect(useHistory().push.mock.calls[0][0]).toBe("/"); // 등록후 메인페이지 옮겼는지 확인
   });
