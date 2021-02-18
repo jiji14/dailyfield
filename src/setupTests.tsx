49b7e8f768f5c14cce752a536e62f3c919700369
Jijeong <jijeong0114@gmail.com>
2021-02-18 10:03:56 +0900

    Record<string, unknown> 관련 주석

--- src/setupTests.tsx
+++ src/setupTests.tsx
@@ -28,6 +28,7 @@ jest.mock("react-router-dom", () => ({
 }));
 
 jest.mock("./globalFunction", () => ({
+  // object를 type으로 쓰면 lint 오류나서 vscode가 추천한 Record<string, unknown>을 type으로 쓰기
   // https://github.com/microsoft/TypeScript/issues/21732
   ...(jest.requireActual("./globalFunction") as Record<string, unknown>),
   deleteReservationStatus: jest.fn(),
