5271149bac13cc02b6118ebe171c48fb5cd83c58
jijeong <jijeong0114@gmail.com>
2021-03-19 15:57:29 +0900

    act 빼기

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, act } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Header from "./Header";
 import firebase from "firebase";
 import { fireAntEvent } from "../setupTests";
