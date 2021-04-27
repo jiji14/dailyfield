a77018f1af8d4e62c07bc4ddb87da62b52079396
jijeong <jijeong0114@gmail.com>
2021-04-28 02:54:50 +0900

    useReservationStatus custom hook 추가

---
+++ src/customHooks/useReservationStatus.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { Match, Status } from "../types";
+import { getReservationStatus } from "../globalFunction";
+import firebase from "firebase";
+
+export function useReservationStatus(
+  match: Match | null,
+  user: firebase.User | null
+): Status {
+  const [reservationStatus, setReservationStatus] = useState<Status>(
+    "신청가능"
+  );
+
+  useEffect(() => {
+    (async () => {
+      setReservationStatus(await getReservationStatus(match, user));
+    })();
+  }, [match, user]);
+
+  return reservationStatus;
+}
