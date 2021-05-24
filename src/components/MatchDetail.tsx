a047bde29a52b9ef85d83ac80f089adbce6dd14d
jijeong <jijeong0114@gmail.com>
2021-05-25 03:01:56 +0900

    직접 입력한 주차 내용 보이게 수정

--- src/components/MatchDetail.tsx
+++ src/components/MatchDetail.tsx
@@ -106,29 +106,6 @@ const MatchDetail = (): JSX.Element | null => {
     }
   };
 
-  const renderPark = () => {
-    if (match?.place.includes("더베이스")) {
-      return (
-        <ul>
-          <li>해주차장 선착순 2명</li>
-          <li>예약신청 후 직접 문의주세요.</li>
-        </ul>
-      );
-    } else if (match?.place.includes("누리")) {
-      return (
-        <ul>
-          <li>입구 주차장 사용가능 (무료)</li>
-          <li>건물 뒷편 주차장 사용 가능 (3대 이상 무료)</li>
-          <li>세화빌딩 지하주차 (무료)</li>
-          <li>(2~3대 가능/단 SUV불가,일반세단 및 경차만 가능)</li>
-          <li>평양면옥 발렛주차(30분 2,000원)</li>
-        </ul>
-      );
-    } else {
-      return <p>직접 문의 해주세요.</p>;
-    }
-  };
-
   const renderCancelPolicy = () => {
     if (match?.isRecurringClass) {
       return (
@@ -225,7 +202,7 @@ const MatchDetail = (): JSX.Element | null => {
         {match.canPark && (
           <>
             <h3 className="title">주차 안내</h3>
-            {renderPark()}
+            {<ReactMarkdown>{match.parkGuide}</ReactMarkdown>}
           </>
         )}
         <h3 className="UlTitle">[운동 사진 & 영상 미리보기]</h3>
